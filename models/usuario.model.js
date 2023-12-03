a2f7e5f1e7b7e6faac88ffc8cdbd8174506c53d1
Fabian <fabianrincon1407@gmail.com>
2023-12-03 00:39:32 -0500

    .

--- models/usuario.model.js
+++ models/usuario.model.js
@@ -3,5 +3,7 @@ import pgService from "../services/pg.services.js";
 export const getUsuario = async (access)=> {
     const pg =  new pgService(); 
     console.log(access)
-    return  await pg.connection.query(`SELECT id FROM usuario where username = $1 and password = $2`, [access.username, access.password]);
+    console.log(await pg.connection.query(`SELECT id FROM usuario where username = $1 and password = $2`, [access.username, access.password]))
+    
+    return 1 ;
 }
